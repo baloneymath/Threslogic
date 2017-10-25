94026aed7c11f773507e3efecea8f7dc727d7842
baloneymath <baloneymath@gmail.com>
2017-10-25 17:56:26 +0800

    fix show distribution

--- scripts/showThresDistrubution.py
+++ scripts/showThresDistrubution.py
@@ -8,16 +8,15 @@ if __name__ == '__main__':
     
     step_size = 1
     tlf_filename = sys.argv[1]
-    total = [0] * 1000
-
+    total = [0] * 1600
     with open(tlf_filename, 'r') as f:
         for line in f:
             tokens = line.split(' ')
             if tokens[0] == '.thres':
                 info = f.readline().split(' ')
                 thres_val = math.ceil(float(info[-1]))
-                total[thres_val // step_size] += 1
-    plt.plot(total)
+                total[thres_val // step_size + 700] += 1
+    plt.plot([x for x in range(-700, 900)], total)
     plt.show()
                 
 
